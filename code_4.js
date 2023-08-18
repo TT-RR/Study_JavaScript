06d5bc1a0118b0d537c498781ebdbb9e8410ee95
TT-RR <kaiza1mage@gmail.com>
2023-08-18 22:18:01 +0900

    繰返し文

---
+++ code_4.js
@@ -0,0 +1,12 @@
+let number = 1;
+
+//繰返し処理(while文)
+while (number <= 100) {
+    console.log(number);
+    number++;
+}
+
+//繰返し処理(for文)
+for (let number = 1; number <= 100; number++) {
+    console.log(number);
+}
